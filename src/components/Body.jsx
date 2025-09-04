d7737700125021809d3633ac442e01cbb88ca473
Enzo Brincalepe <enzo.brincalepe@gmail.com>
2025-09-04 16:42:26 -0300

    add todos os produtos

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -6,6 +6,7 @@ import Card from './Card';
 function Body(){
     const [roupas, setRoupas] = useState([]);
     const [produtosDeBeleza, setProdutosDeBeleza] = useState([]);
+    const [itensParaCasa, setItenParaCasa] = useState([]);
 
     useEffect(() => {
         fetch('/produtos.json')
@@ -13,6 +14,7 @@ function Body(){
             .then(dados => {
                 setRoupas(dados.roupas);
                 setProdutosDeBeleza(dados.produtosDeBeleza);
+                setItenParaCasa(dados.itensParaCasa);
             })
             .catch(err => console.error("Erro ao carregar produtos:", err));
     }, []);
@@ -22,28 +24,48 @@ function Body(){
             <img src={Banner} alt="banner" className='pqp'/>
             <h1 className='body-title'>Nossas Roupas</h1>
             <p className='body-text'>Descubra nossa coleção de roupas sustentáveis e eco-friendly.</p>
-    <div className='coluna'>
             <div className='body-cards'>
-                {roupas.map((roupa) => (
-                    <Card 
-                        key={roupa.id}
-                        nome={roupa.nome}
-                        preco={roupa.preco}
-                        img={roupa.img}
-                    />
-                ))}
+                <div className='row'>
+                    {roupas.map((roupa) => (
+                        <Card 
+                            key={roupa.id}
+                            nome={roupa.nome}
+                            preco={roupa.preco}
+                            img={roupa.img}
+                        />
+                    ))}
+                </div>
+                </div>
+            
             <h1 className='body-title'>Nossos Produtos de Beleza</h1>
             <p className='body-text'>Descubra nossa coleção de roupas sustentáveis e eco-friendly.</p>
-                {produtosDeBeleza.map((beleza) => (
-                    <Card
-                        key={beleza.id}
-                        nome={beleza.nome}
-                        preco={beleza.preco}
-                        img={beleza.img}
-                    />
-                ))}
-    </div>
+            <div className='body-cards'>
+                <div className='row'>
+                    {produtosDeBeleza.map((beleza) => (
+                        <Card
+                            key={beleza.id}
+                            nome={beleza.nome}
+                            preco={beleza.preco}
+                            img={beleza.img}
+                        />
+                    ))} 
+                </div>
             </div>
+                <h1 className='body-title'>Nossos Produtos de Beleza</h1>
+                <p className='body-text'>Descubra nossa coleção de roupas sustentáveis e eco-friendly.</p>
+                <div className='body-cards'>
+                <div className='row'>
+                {itensParaCasa.map((casa) => (
+                        <Card
+                            key={casa.id}
+                            nome={casa.nome}
+                            preco={casa.preco}
+                            img={casa.img}
+                        />
+                    ))} 
+                    </div>
+                </div>
+            
         </main>
     );
 }
